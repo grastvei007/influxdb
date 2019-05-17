9f4dc75ae2d8a8c1704b972ae76a6f0c5577c37a
Stein Gunnar Grastveit <grastvei@pvv.org>
2019-05-17 21:04:13 +0200

    add some settings

--- dbdata.cpp
+++ dbdata.cpp
@@ -8,17 +8,17 @@ DbData::DbData()
 
 void DbData::setDataBaseName(QString aDbName)
 {
-
+    mDbName = aDbName;
 }
 
 
 void DbData::setServerIp(QString aIp)
 {
-
+    mServerIp = aIp;
 }
 
 
 void DbData::setServerPort(QString aPort)
 {
-
+    mServerPort = aPort;
 }
